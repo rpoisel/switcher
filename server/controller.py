df86d504eebdee2ac83883257e601a06c083d3b0
Rainer Poisel <rainer.poisel@gmail.com>
2013-11-13 22:08:25 +0100

    first minimal server version running

---
+++ server/controller.py
@@ -0,0 +1,67 @@
+import sys
+
+from gevent import monkey
+#from socketio import socketio_manage
+from socketio.server import SocketIOServer
+#from werkzeug.wsgi import SharedDataMiddleware
+
+from flask import Flask  # , request, render_template
+from flask.ext.restful import Api
+
+#from rangemanager import RangeManager
+from resources import FetchRange, Report, Progress, Reset
+#from namespaces import PushNamespace
+
+
+def main():
+    try:
+        # init
+#        lModel = RangeManager(int(sys.argv[1]), int(sys.argv[2]))
+
+        monkey.patch_all()
+        lApp = Flask(__name__)
+        lApp.debug = True
+        lApi = Api(lApp)
+#        FetchRange.sModel = lModel
+#        Report.sModel = lModel
+#        Progress.sModel = lModel
+#        Reset.sModel = lModel
+#        PushNamespace.sModel = lModel
+
+        # routes
+        lApp.add_url_rule('/',                      'poll', poll)
+#        lApp.add_url_rule('/socket.io/<path:path>', 'socket.io', run_socketio)
+        lApi.add_resource(FetchRange,               '/fetch')
+        lApi.add_resource(Report,                   '/report')
+        lApi.add_resource(Progress,                 '/progress')
+        lApi.add_resource(Reset,                    '/reset')
+
+        # go
+#        lApp = SharedDataMiddleware(lApp, {})
+        lServer = SocketIOServer(
+            ('0.0.0.0', 5000),
+            lApp,
+            namespace="socket.io",
+            policy_server=False)
+        lServer.serve_forever()
+    except IndexError:
+        print "Usage: " + sys.argv[0] + \
+            " <number of ranges> <timeout per range>"
+    except KeyboardInterrupt:
+        lServer.stop()
+
+
+def poll():
+    return ""
+#    return render_template('progress.html')
+
+
+#def run_socketio(path):
+#    socketio_manage(request.environ, {'': PushNamespace}, path)
+#    # workaround to omit the "ValueError: View function did not
+#    #     return a response" exception
+#    return render_template('dummy.html')
+
+
+if __name__ == '__main__':
+    main()
---
+++ server/resources.py
@@ -0,0 +1,41 @@
+from flask.ext.restful import Resource
+from flask.ext.restful.reqparse import RequestParser
+
+
+class FetchRange(Resource):
+
+    def get(self):
+        return {'eins': 'zwo'}
+
+
+class Report(Resource):
+
+    def __init__(self):
+        super(Report, self).__init__()
+        self.__mParser = RequestParser()
+        self.__mParser.add_argument('success', type=str, required=True)
+        self.__mParser.add_argument('message', type=str)
+        self.__mParser.add_argument('rangeId', type=int, required=True)
+
+    def get(self):
+        lArgs = self.__mParser.parse_args()
+        lSuccess = lArgs['success']
+        lMessage = lArgs['message']
+        lRangeId = lArgs['rangeId']
+        return {}
+
+
+class Progress(Resource):
+    def get(self):
+        return {
+            'value': 5,
+            'message': "Not so interesting"
+        }
+
+
+class Reset(Resource):
+    def get(self):
+        return {}
+
+
+
